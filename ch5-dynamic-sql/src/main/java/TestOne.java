6ee0e89d2c0ca0e457f61e8b020840a3e247c17b
xiaowenbin <xiaowenbin@ylmf1000.onexmail.com>
2022-01-19 11:18:56 +0800

    feat:dynamic sql

---
+++ ch5-dynamic-sql/src/main/java/TestOne.java
@@ -0,0 +1,55 @@
+import dao.StudentDao;
+import domain.QueryParam;
+import domain.Student;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+import utils.MyBatisUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TestOne {
+    @Test
+    public void test01() {
+        SqlSession sqlSession = MyBatisUtils.getSession();
+        StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+        Student student = new Student();
+        student.setAge(20);
+        student.setName("王五");
+        List<Student> students = studentDao.selectStudentIf(student);
+        for (Student stu : students) {
+            System.out.println(stu);
+        }
+    }
+    @Test
+    public void test02() {
+        SqlSession sqlSession = MyBatisUtils.getSession();
+        StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+        Student student = new Student();
+        student.setName("王五");
+        student.setAge(30);
+        studentDao.selectStudentWhere(student);
+    }
+    @Test
+    public void test03() {
+        SqlSession sqlSession = MyBatisUtils.getSession();
+        StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+        List<Integer> stuIdList = new ArrayList<>();
+        stuIdList.add(1);
+        stuIdList.add(66);
+        stuIdList.add(2);
+        studentDao.selectForeachOne(stuIdList);
+    }
+    @Test
+    public void test04() {
+        SqlSession sqlSession = MyBatisUtils.getSession();
+        StudentDao studentDao = sqlSession.getMapper(StudentDao.class);
+        List<Student> stuList = new ArrayList<>();
+        stuList.add(new Student(1));
+        stuList.add(new Student(2));
+        stuList.add(new Student(66));
+        studentDao.selectForeachTwo(stuList);
+    }
+}
---
+++ ch5-dynamic-sql/src/main/java/dao/StudentDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import domain.QueryParam;
+import domain.Student;
+import org.apache.ibatis.annotations.Param;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+public interface StudentDao {
+    //动态sql ，使用java对象作为参数
+    List<Student> selectStudentIf(Student student);
+
+    //where使用
+    List<Student> selectStudentWhere(Student student);
+
+    //foreach 用法 1
+    List<Student> selectForeachOne(List<Integer> idlist);
+
+    //foreach 用法 2
+    List<Student> selectForeachTwo(List<Student> stulist);
+}
